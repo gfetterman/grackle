01d5a9e8ac27b9b678f4ee1b68a602af14aaacfc
Graham Fetterman <graham.fetterman@gmail.com>
2018-11-12 19:05:02 -0600

    Refactor read, eval, & print

--- interp.c
+++ interp.c
@@ -8,24 +8,19 @@
 #define EXIT_KEYWORD "(exit)"
 
 typedef struct LLNODE {
+    unsigned int symbol_number;
     char* symbol;
     char* value;
     struct LLNODE* next;
 } Node;
 
-typedef struct TOKENLIST {
-    unsigned int num_tokens;
-    int* token_starts;
-    char* full_string;
-    unsigned int str_len;
-} TokenList;
-
-Node* create_node(char* symbol, char* value) {
+Node* create_node(char* symbol, char* value, unsigned int symbol_number) {
     Node* new_node = malloc(sizeof(Node));
     if (new_node == NULL) {
         fprintf(stderr, "fatal error: malloc failed in create_node()\n");
         exit(-1);
     }
+    new_node->symbol_number = symbol_number;
     new_node->symbol = symbol;
     new_node->value = value;
     new_node->next = NULL;
@@ -43,16 +38,38 @@ Node* search_list(Node* list, char* symbol) {
     return NULL;
 }
 
-void install_symbol(Node** list, char* symbol, char* value) {
+Node* symbol_from_index(Node* list, unsigned int index) {
+    Node* curr = list;
+    while (curr != NULL) {
+        if (curr->symbol_number == index) {
+            break;
+        }
+        curr = curr->next;
+    }
+    return curr;
+}
+
+unsigned int len(Node* list) {
+    unsigned int length = 0;
+    while (list != NULL) {
+        list = list->next;
+        length++;
+    }
+    return length;
+}
+
+unsigned int install_symbol(Node** list, char* symbol, char* value) {
     Node* existing_value = search_list(*list, symbol);
     if (existing_value == NULL) {
-        Node* new_node = create_node(symbol, value);
+        unsigned int num = len(*list);
+        Node* new_node = create_node(symbol, value, num);
         new_node->next = *list;
         *list = new_node;
+        return num;
     } else {
         existing_value->value = value;
+        return existing_value->symbol_number;
     }
-    return;
 }
 
 char* substring(char* str, unsigned int start, unsigned int end) {
@@ -74,130 +91,165 @@ char* substring(char* str, unsigned int start, unsigned int end) {
     return ss;
 }
 
-bool substring_match(char* str1, char* str2, unsigned int start, unsigned int end) {
-    // str1 must be of length at least end
-    // str2 must be of length exactly (end - start)
-    // returns 1 for match, 0 for no match
-    if (strlen(str1) < end || strlen(str2) != (end - start)) {
-        return 0;
-    }
-    bool match = 1;
-    for (int i = 0; i < (end - start); i++) {
-        match &= (str1[start + i] == str2[i]);
-    }
-    return match;
-}
-
-TokenList* tokenize(char* str) {
-    TokenList* tklist = malloc(sizeof(TokenList));
-    if (tklist == NULL) {
-        fprintf(stderr, "fatal error: malloc failed in tokenize()\n");
-        exit(-1);
-    }
-    char c;
-    unsigned int num_tokens = 1;
-    char* full_string = str;
-    while((c = *full_string++)) {
-        num_tokens += (c == ' ') ? 1 : 0;
-    }
-    tklist->num_tokens = num_tokens;
-    tklist->full_string = str;
-    tklist->str_len = strlen(str);
-    tklist->token_starts = malloc(sizeof(int) * num_tokens);
-    if (tklist->token_starts == NULL) {
-        fprintf(stderr, "fatal error: malloc failed in tokenize()\n");
-        exit(-1);
-    }
-    tklist->token_starts[0] = 0;
-    num_tokens = 1;
-    for (int i = 0; i < tklist->str_len; i++) {
-        if (str[i] == ' ') {
-            tklist->token_starts[num_tokens] = i;
-            num_tokens++;
-        }
-    }
-    return tklist;
-}
-
-void print_tokens(TokenList* tklist) {
-    if (tklist != NULL) {
-        char* temp;
-        printf("whole string: %s\n", tklist->full_string);
-        printf("number of tokens: %d\n", (int)tklist->num_tokens);
-        for (int i = 0; i < tklist->num_tokens; i++) {
-            printf("  token %d\n", i);
-            printf("    start: %d\n", tklist->token_starts[i]);
-            printf("    end: %d\n", (i == tklist->num_tokens - 1) ? tklist->str_len : tklist->token_starts[i + 1]);
-            temp = substring(tklist->full_string, \
-                             tklist->token_starts[i], \
-                             (i == tklist->num_tokens - 1) ? tklist->str_len : tklist->token_starts[i + 1]);
-            printf("    substring: %s\n", temp);
-            free(temp);
-        }
-    }
-    return;
-}
-
 void print_symbol_table(Node* list) {
     Node* curr = list;
     printf("current symbol table:\n");
     while (curr != NULL) {
-        printf("  symbol \"%s\" has value \"%s\"\n", curr->symbol, curr->value);
+        printf("  symbol #%d, \"%s\", has value \"%s\"\n", curr->symbol_number, curr->symbol, curr->value);
         curr = curr->next;
     }
     return;
+}
+
+unsigned int pull_token(Node** symbol_table, \
+                        char str[], \
+                        unsigned int length, \
+                        unsigned int* offset) {
+    // length must == length of character array str
+    // pulls one token out of the front of str
+    // if it reaches the end of str, it returns 0
+    // if it's '(' or ')', it returns 1 or 2, respectively
+    // if it's anything else, it returns an index into
+    // the symbol table (here, for the moment, the number
+    // table and symbol table have been merged)
+    // leading spaces are ignored
+    // (it can't handle other whitespace at the moment)
+    //printf("string to tokenize: \"%s\", beginning with '%c'\n", str, str[*offset]);
+    while (*offset < length && str[*offset] == ' ') {
+        (*offset)++;
+    }
+    if (*offset >= length) { // reached end of input
+        return 0;
+    } else if (str[*offset] == '(') { // open parenthesis
+        (*offset)++;
+        return 1;
+    } else if (str[*offset] == ')') { // close parenthesis
+        (*offset)++;
+        return 2;
+    } else { // number or atom
+        unsigned int token_len = 0;
+        while (*offset + token_len < length && \
+               str[*offset + token_len] != ' ' && \
+               str[*offset + token_len] != '(' && \
+               str[*offset + token_len] != ')') {
+            token_len++;
+        }
+        char* token_str = substring(str, *offset, *offset + token_len);
+        //printf("symbol found: %s\n", token_str);
+        Node* found = search_list(*symbol_table, token_str);
+        (*offset) += token_len; // for the next call to pull_token()
+        if (found == NULL) {
+            unsigned int new_offset = install_symbol(symbol_table, token_str, token_str);
+            //printf("new symbol added to symbol table at position %d\n", new_offset);
+            //print_symbol_table(*symbol_table);
+            // and now it's the symbol table's problem to free the memory
+            // allocated for token_str
+            return new_offset;
+        } else {
+            free(token_str);
+            return found->symbol_number;
+        }
+    }
+}
+
+void setup_symbol_table(Node** symbol_table) {
+    install_symbol(symbol_table, "NULL_SENTINEL", "NULL_SENTINEL");
+    install_symbol(symbol_table, "(", "(");
+    install_symbol(symbol_table, ")", ")");
+    install_symbol(symbol_table, "+", "+");
+    install_symbol(symbol_table, "exit", "exit");
+    return;
+}
+
+void get_input(char* prompt, char buffer[], unsigned int buffer_size) {
+    printf("%s ", prompt);
+    fgets(buffer, buffer_size, stdin); // yes, this is unsafe
+    // drop newline at end of input
+    if ((strlen(buffer) > 0) && (buffer[strlen(buffer) - 1] == '\n')) {
+        buffer[strlen(buffer) - 1] = '\0';
+    }
+    return;
+}
+
+unsigned int evaluate(Node** symbol_table, \
+                      unsigned int token_list[], \
+                      unsigned int num_tokens) {
+    // initial version doesn't support nesting
+    unsigned int finger = 0;
+    unsigned int result = 0;
+    while (finger < num_tokens) {
+        switch (token_list[finger]) {
+            case 0: // end of token list
+                finger = num_tokens;
+                break;
+            case 1: // open parenthesis
+                finger++;
+                break;
+            case 2: // close parenthesis
+                finger = num_tokens;
+                break;
+            case 3: {// addition
+                unsigned int operand_1, operand_2;
+                Node* found = symbol_from_index(*symbol_table, token_list[finger + 1]);
+                if (found == NULL) {
+                    printf("error: symbol \"%d\" not found\n", token_list[finger + 1]);
+                    finger = num_tokens;
+                    break;
+                } else {
+                    operand_1 = atoi(found->value);
+                }
+                found = symbol_from_index(*symbol_table, token_list[finger + 2]);
+                if (found == NULL) {
+                    printf("error: symbol \"%d\" not found\n", token_list[finger + 2]);
+                    finger = num_tokens;
+                    break;
+                } else {
+                    operand_2 = atoi(found->value);
+                }
+                result = operand_1 + operand_2;
+                finger += 3;
+                break;
+            }
+            case 4:
+                printf("exiting...\n");
+                exit(0);
+            default: {
+                Node* found = symbol_from_index(*symbol_table, token_list[finger]);
+                if (found == NULL) {
+                    printf("error: token index \"%d\" unrecognized\n", token_list[finger]);
+                } else {
+                    result = atoi(found->value);
+                }
+            }
+        }
+    }
+    return result;
 }
 
 int main() {
     bool exit = 0;
     char input[BUF_SIZE];
     Node* symbol_table = NULL;
+    setup_symbol_table(&symbol_table);
+    unsigned int token_list[20]; // this is a temporary solution
     while (!exit) {
-        printf("%s ", PROMPT);
-        fgets(input, BUF_SIZE, stdin); // YES I KNOW this is unsafe
-        if ((strlen(input) > 0) && (input[strlen(input) - 1] == '\n')) {
-            input[strlen(input) - 1] = '\0';
+        // clear token list
+        for (int i = 0; i < 20; i++) {
+            token_list[i] = 0;
         }
-        TokenList* tklist = tokenize(input);
-        //print_tokens(tklist);
-        if (!strcmp(EXIT_KEYWORD, input)) {
-            exit = 1;
-        } else if (tklist->num_tokens > 0 && \
-                   substring_match(input, \
-                                   "(SETQ", \
-                                   tklist->token_starts[0], \
-                                   (tklist->num_tokens > 1) ? tklist->token_starts[1] : tklist->str_len)) {
-            if (tklist->num_tokens == 3) {
-                // substring allocates the string locations, and those strings
-                // are now the symbol table's responsibility to free when necessary
-                install_symbol(&symbol_table,
-                               substring(input, tklist->token_starts[1], tklist->token_starts[2]), \
-                               substring(input, tklist->token_starts[2], tklist->str_len));
-            } else {
-                fprintf(stderr, "\"SETQ\" takes exactly 2 arguments; %d given\n", tklist->num_tokens);
-            } 
-        } else if (tklist->num_tokens > 0 && \
-                   substring_match(input, \
-                                   "(QUOTE", \
-                                   tklist->token_starts[0], \
-                                   (tklist->num_tokens > 1) ? tklist->token_starts[1] : tklist->str_len)) {
-            if (tklist->num_tokens == 2) {
-                char* symbol = substring(input, \
-                                         tklist->token_starts[1], tklist->str_len);
-                Node* lookup = search_list(symbol_table, symbol);
-                if (lookup == NULL) {
-                    fprintf(stderr, "error: \"%s\" not in symbol table\n", symbol);
-                } else {
-                    printf("value of %s = %s\n", lookup->symbol, lookup->value);
-                }
-                free(symbol);
-            } else {
-                fprintf(stderr, "\"QUOTE\" takes exactly 1 argument; %d given\n", tklist->num_tokens);
+        get_input(PROMPT, input, BUF_SIZE);
+        unsigned int token;
+        unsigned int num_tokens = 0;
+        unsigned int offset = 0;
+        while ((token = pull_token(&symbol_table, input, strlen(input), &offset))) {
+            if (num_tokens >= 20) {
+                printf("error: cannot parse more than 20 tokens\n");
+                break; // then evaluation will probably fail and throw another error
             }
-        } else {
-            printf("%s\n", input);
+            token_list[num_tokens] = token;
+            num_tokens++;
         }
-        //print_symbol_table(symbol_table);
+        printf("result = %d\n", evaluate(&symbol_table, token_list, num_tokens));
     }
     return 0;
 }
