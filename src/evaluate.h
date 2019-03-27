37aab920a99eb5e88970ea6f2d04a790c9717a39
Graham Fetterman <graham.fetterman@gmail.com>
2019-03-27 16:49:16 -0500

    Rearrange declarations

--- src/evaluate.h
+++ src/evaluate.h
@@ -9,11 +9,7 @@
 #include "environment.h"
 #include "grackle_utils.h"
 
-typed_ptr* collect_args(const s_expr* se, \
-                        environment* env, \
-                        int min_args, \
-                        int max_args, \
-                        bool evaluate_all_args);
+typed_ptr* evaluate(const s_expr* se, environment* env);
 
 typed_ptr* eval_arithmetic(const s_expr* se, environment* env);
 typed_ptr* eval_comparison(const s_expr* se, environment* env);
@@ -26,13 +22,17 @@ typed_ptr* eval_atom_pred(const s_expr* se, environment* env, type t);
 typed_ptr* eval_list_construction(const s_expr* se, environment* env);
 typed_ptr* eval_cond(const s_expr* se, environment* env);
 
-sym_tab_node* collect_parameters(typed_ptr* tp, environment* env);
 typed_ptr* eval_lambda(const s_expr* se, environment* env);
+sym_tab_node* collect_parameters(typed_ptr* tp, environment* env);
 
+typed_ptr* eval_user_function(const s_expr* se, environment* env);
 sym_tab_node* bind_args(environment* env, fun_tab_node* ftn, typed_ptr* args);
 environment* make_eval_env(environment* env, sym_tab_node* bound_args);
-typed_ptr* eval_user_function(const s_expr* se, environment* env);
 
-typed_ptr* evaluate(const s_expr* se, environment* env);
+typed_ptr* collect_args(const s_expr* se, \
+                        environment* env, \
+                        int min_args, \
+                        int max_args, \
+                        bool evaluate_all_args);
 
 #endif
