642417559630203b885fd6bcc83f9e110839e80f
Graham Fetterman <graham.fetterman@gmail.com>
2019-07-24 23:03:38 -0500

    Travis debug 14

--- tests/unit_tests_environment.c
+++ tests/unit_tests_environment.c
@@ -622,9 +622,15 @@ void test_function_lookup_index(test_env* te) {
     Environment* env = create_environment(0, 0);
     char* x = "x-param";
     char* y = "y-param";
+    char* x_cpy = malloc(sizeof(char) * 20);
+    char* y_cpy = malloc(sizeof(char) * 20);
+    for (unsigned int i = 0; i < 8; i++) {
+        x_cpy[i] = x[i];
+        y_cpy[i] = y[i];
+    }
     Symbol_Node* args;
-    args = create_symbol_node(0, strdup(x), TYPE_NUM, (tp_value){.idx=TEST_NUM});
-    args->next = create_symbol_node(1, strdup(y), TYPE_BOOL, (tp_value){.idx=TEST_NUM});
+    args = create_symbol_node(0, x_cpy, TYPE_NUM, (tp_value){.idx=TEST_NUM});
+    args->next = create_symbol_node(1, y_cpy, TYPE_BOOL, (tp_value){.idx=TEST_NUM});
     Environment* closure = create_environment(0, 0);
     //Environment* closure = copy_environment(env);
     typed_ptr* body = create_s_expr_tp(create_empty_s_expr());
