83dda8e16b5de9c7b447b82152da4300a96cd2e3
Graham Fetterman <graham.fetterman@gmail.com>
2019-09-15 14:04:44 -0500

    Enable bare symbols and multiple inputs per line

--- src/grackle.c
+++ src/grackle.c
@@ -19,25 +19,34 @@ int main() {
             printf("\n");
         } else if (parse_output->type == TYPE_S_EXPR && \
                    !is_empty_list(parse_output->ptr.se_ptr)) {
-            typed_ptr* eval_output = evaluate(parse_output, env);
-            print_typed_ptr(eval_output, env);
-            printf("\n");
-            if (eval_output->type == TYPE_ERROR && \
-                eval_output->ptr.idx == EVAL_ERROR_EXIT) {
-                exit = true;
+            for (s_expr* term = parse_output->ptr.se_ptr; \
+                 !is_empty_list(term); \
+                 term = term->cdr->ptr.se_ptr) {
+                typed_ptr* eval_output = evaluate(term->car, env);
+                print_typed_ptr(eval_output, env);
+                printf("\n");
+                if (eval_output->type == TYPE_ERROR && \
+                    eval_output->ptr.idx == EVAL_ERROR_EXIT) {
+                    exit = true;
+                }
+                if (term->car->type == TYPE_S_EXPR) {
+                    delete_s_expr_recursive(term->car->ptr.se_ptr, true);
+                }
+                if (eval_output->type == TYPE_S_EXPR) {
+                    delete_s_expr_recursive(eval_output->ptr.se_ptr, true);
+                } else if (eval_output->type == TYPE_STRING) {
+                    delete_string(eval_output->ptr.string);
+                }
+                if (eval_output->type == TYPE_ERROR) {
+                    free(eval_output);
+                    break;
+                }
+                free(eval_output);
             }
-            delete_s_expr_recursive(parse_output->ptr.se_ptr, true);
-            if (eval_output->type == TYPE_S_EXPR) {
-                delete_s_expr_recursive(eval_output->ptr.se_ptr, true);
-            } else if (eval_output->type == TYPE_STRING) {
-                delete_string(eval_output->ptr.string);
-            }
-            free(eval_output);
         }
         free(parse_output);
         free(input);
     }
     delete_environment(env);
-    printf("exiting...\n");
     return 0;
 }
